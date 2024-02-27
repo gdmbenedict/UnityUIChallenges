290cdd2614773bca222d956959ed355d69505b90
Matthieu Benedict <97464794+Twitchton@users.noreply.github.com>
2024-02-27 13:38:22 -0400

    Added RGB color sliders
    Added RGB color sliders to the game.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
@@ -20,17 +21,31 @@ public class UIManager : MonoBehaviour
     public GameObject hidingImage;
     public TextMeshProUGUI hideButtonText;
 
+    [Header("RGB Sliders")]
+    public Image rgbImage;
+    public Slider redSlider;
+    public Slider greenSlider;
+    public Slider blueSlider;
+    public Slider alphaSlider;
+
     //start function called on first frame
     void Start()
     {
         hidingImage.SetActive(false);
         SetHideButtonText();
+
+        //setting value of sliders to 1
+        redSlider.value = 1;
+        greenSlider.value = 1;
+        blueSlider.value = 1;
+        alphaSlider.value = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
         PrintScore();
+        UpdateRGBPicture();
     }
 
     //Score Handling functions
@@ -87,5 +102,24 @@ public class UIManager : MonoBehaviour
         }
 
         hideButtonText.text = buttonAction + " Cookies";
+    }
+
+    //Exit Button
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    //RGB Slider
+    private void UpdateRGBPicture()
+    {
+        Color color = rgbImage.color;
+
+        color.r = redSlider.value;
+        color.g = greenSlider.value;
+        color.b = blueSlider.value;
+        color.a = alphaSlider.value;
+
+        rgbImage.color = color;
     }
 }
